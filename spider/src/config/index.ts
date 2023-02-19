d41516f8090c64f1caac410c17496aaa8dbb32f8
hw0505 <39039541+hw0505@users.noreply.github.com>
2023-02-19 13:04:37 +0800

    Update index.ts

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -5,7 +5,7 @@ export const config = dotenv.config();
 export const organiztion: string = 'os-lecture';            // 组织
 export const fullOrganization: string = '123707571-os-lecture-classroom'; // classroom名称
 export const assignment = '2023S-uCore';   // assignment
-export const works = ['ch3','ch4']; // assignment 的不同情况
+export const works = ['ch3','ch4','ch5']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 
 // JsonData store the grades and the other info.
