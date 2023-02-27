030c52ee47b14ab3d6f444c69fe89622e1813d95
hw0505 <39039541+hw0505@users.noreply.github.com>
2023-02-27 21:00:49 +0800

    update classroom info

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -2,10 +2,10 @@ import * as dotenv from "dotenv";
 
 export const config = dotenv.config();
 
-export const organiztion: string = 'os-lecture';            // 组织
-export const fullOrganization: string = '123707571-os-lecture-classroom'; // classroom名称
+export const organiztion: string = 'LearningOS';            // 组织
+export const fullOrganization: string = '19380377-2023s-os-ucore-classroom'; // classroom名称
 export const assignment = '2023S-uCore';   // assignment
-export const works = ['ch3','ch4','ch5']; // assignment 的不同情况
+export const works = ['ch3','ch4','ch5','ch6','ch8']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 
 // JsonData store the grades and the other info.
